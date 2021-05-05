8352af0c2f2054768f9abc6c703100f3f7b78def
ZarmDev <>
2021-05-05 17:56:50 +0000

    More CSS

--- script.js
+++ script.js
@@ -31,3 +31,17 @@ for (var i = 0; i < bottomFooter.children.length; i++) {
   bottomFooter.children[i].addEventListener('pointerenter', footerhover)
   bottomFooter.children[i].addEventListener('pointerleave', footerhoverout)
 }
+
+var storyslist = [['https://www.youtube.com/watch?v=94ENNlF_05k', 'Delete Google from Your Life Now']];
+var videoslist = [['https://www.youtube.com/watch?v=94ENNlF_05k', 'Delete Google from Your Life Now']];
+var youtubechannelslist = [['https://www.youtube.com/user/thejuicemedia/featured', 'The Juice Media'], ['https://www.youtube.com/channel/UCjr2bPAyPV7t35MvcgT3W8Q', 'The Hated One']];
+
+for (var s = 0; s < storyslist.length; s++) {
+  storys.insertAdjacentHTML('beforeend', `<div class="channelLink" style="background-color: rgb(211, 210, 210);"><a style="color: black;" href="${storyslist[s][0]}">${storyslist[s][1]}</a></div>`)
+}
+for (var v = 0; v < videoslist.length; v++) {
+  videos.insertAdjacentHTML('beforeend', `<div class="channelLink" style="background-color: rgb(253, 136, 136);"><a style="color: rgb(122, 94, 94);" href="${videoslist[v][0]}">${videoslist[v][1]}</a></div>`)
+}
+for (var yc = 0; yc < youtubechannelslist.length; yc++) {
+  document.getElementsByClassName('channels')[0].insertAdjacentHTML('beforeend', `<div class="channelLink" style="background-color: rgb(219, 22, 22);"><a style="color: rgb(255, 231, 231);" href="${youtubechannelslist[yc][0]}">${youtubechannelslist[yc][1]}</a></div>`)
+}
