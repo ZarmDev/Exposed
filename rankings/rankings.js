f42203f1ea19e12e8efc61c4097b612d1649acdb
ZarmDev <>
2021-05-08 15:55:31 +0000

    Rankings

--- rankings/rankings.js
+++ rankings/rankings.js
@@ -35,14 +35,38 @@ for (var i = 0; i < bottomFooter.children.length; i++) {
   bottomFooter.children[i].addEventListener('pointerleave', footerhoverout)
 }
 
-var rankingListsArray = [['Most Climate Action', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi']];
+var rankingListsArray = [['Most Climate Action', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi'], ['Most Climate Action', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi', '1. Hello', '2. Hi']];
+
+
+
+window.rankTrackDiv = -1;
+
+window.toggled = false;
+
+function hideDropdown(dropdown) {
+  if (window.toggled === false) {
+    for (var hd = 0; hd < document.getElementsByClassName(`dropdown${window.rankTrackDiv}`).length; hd++) {
+      document.getElementsByClassName(`dropdown${dropdown}`)[hd].style.display = 'none';
+    }
+    document.getElementsByTagName('i')[dropdown].style = `transform: rotate(180deg); margin-bottom: 2vw;`;
+    window.toggled = true;
+  } else {
+    for (var hd = 0; hd < document.getElementsByClassName(`dropdown${window.rankTrackDiv}`).length; hd++) {
+      document.getElementsByClassName(`dropdown${dropdown}`)[hd].style.display = '';
+    }
+    document.getElementsByTagName('i')[dropdown].style = `transform: rotate(0deg); margin-bottom: 2vw;`;
+    window.toggled = false;
+  }
+}
+
 
 for (var rla = 0; rla < rankingListsArray.length; rla++) {
+  window.rankTrackDiv++
   for (var il = 0; il < rankingListsArray[rla].length; il++) {
     if (il === 0) {
-      rankingLists.insertAdjacentHTML('beforeend', `<h1>${rankingListsArray[rla][0]}</h1>`)
+      rankingLists.insertAdjacentHTML('beforeend', `<h1>${rankingListsArray[rla][0]}</h1><i class="fa fa-sort-desc" aria-hidden="true" onclick="hideDropdown(${rankTrackDiv})"></i><br>`)
     } else {
-      rankingLists.insertAdjacentHTML('beforeend', `<li>${rankingListsArray[rla][il]}<br><progress value="${1 - il / rankingListsArray[rla].length}"></progress></li>`)
+      rankingLists.insertAdjacentHTML('beforeend', `<li class="dropdown${window.rankTrackDiv}">${rankingListsArray[rla][il]}<br><progress value="${1 - il / rankingListsArray[rla].length}"></progress></li>`)
     }
   }
 }
